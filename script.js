2fa002d04b0cdc2e4b2072801ad776431cffaed7
Henrique Prado <henrique.mxnero@gmail.com>
2021-07-23 12:50:56 -0300

    Identificando os eventos de mousedown, mousemove e mouseup na área do Canvas

--- script.js
+++ script.js
@@ -1,9 +1,21 @@
+// Initial Data
 let currentColor = 'black';
+let screen = document.querySelector('$tela');
+let ctx = screen.getContext('2d');
 
+
+// Events
 document.querySelectorAll('.colorArea .color').forEach(item => {
     item.addEventListener('click', colorClickEvent);
 });
 
+screen.addEventListener('mousedown', mouseDownEvent);
+screen.addEventListener('mousemove', mouseMoveEvent);
+screen.addEventListener('mouseup', mouseUpEvent);
+
+
+
+// Functions
 function colorClickEvent(event) {
     let color = event.target.getAttribute('data-color');
     currentColor = color;
@@ -11,3 +23,17 @@ function colorClickEvent(event) {
     document.querySelector('.color.active').classList.remove('active');
     event.target.classList.add('active');
 }
+
+
+function mouseDownEvent() {
+
+}
+
+function mouseMoveEvent() {
+
+
+}
+
+function mouseUpEvent() [
+
+]
